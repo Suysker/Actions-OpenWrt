280dd920b52b69909dee67a301fc89418dfea178
Suysker <61983976+Suysker@users.noreply.github.com>
2022-12-09 21:24:38 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,3 +18,4 @@
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '1a src-git small https://github.com/kenzok8/small' feeds.conf.default
+sed -i '1i src-git sbwml https://github.com/sbwml/luci-app-mosdns' feeds.conf.default
