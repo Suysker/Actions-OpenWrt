3343209ec59488995a560e211098922fa2f9b742
Suysker <61983976+Suysker@users.noreply.github.com>
2023-04-11 21:32:39 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -46,7 +46,7 @@ sed -i 's/BASE_TAG=v2.*/BASE_TAG=v2.6.12/' feeds/packages/haproxy/get-latest-pat
 sed -i 's/-b 0.0.0.0 -s/-b 0.0.0.0 -B :: -j 2 -s/' feeds/kenzo/luci-app-passwall/root/usr/share/passwall/app.sh
 
 
-sed -i '/log                     global/a\    option                  tcp-check' feeds/kenzo/luci-app-passwall/root/usr/share/passwall/haproxy.lua
+sed -i '/log                     global/a\	option                  tcp-check' feeds/kenzo/luci-app-passwall/root/usr/share/passwall/haproxy.lua
 #sed -i 's/daemon/daemon\n              nbproc      4\n              nbthread    2/' feeds/kenzo/luci-app-passwall/root/usr/share/passwall/app.sh
 #请求失败重试次数
 sed -i 's/retries                 2/retries                 1/' feeds/kenzo/luci-app-passwall/root/usr/share/passwall/haproxy.lua
@@ -63,9 +63,9 @@ sed -i 's/timeout server          1m/timeout server          6m/' feeds/kenzo/lu
 #最大并发连接数
 #sed -i 's/maxconn                 3000/maxconn                 6000/' feeds/kenzo/luci-app-passwall/root/usr/share/passwall/haproxy.lua
 #rise 3是3次正确认为服务器可用，fall 3是3次失败认为服务器不可用
-sed -i 's/inter 1500 rise 1 fall 3/inter 1000 rise 30 fall 3/' feeds/kenzo/luci-app-passwall/root/usr/share/passwall/haproxy.lua
+#sed -i 's/inter 1500 rise 1 fall 3/inter 1000 rise 30 fall 3/' feeds/kenzo/luci-app-passwall/root/usr/share/passwall/haproxy.lua
 
-sed -i 's/inter 1000 rise 30 fall 3 \%s/& on-marked-down shutdown-sessions/' feeds/kenzo/luci-app-passwall/root/usr/share/passwall/haproxy.lua
+sed -i 's/rise 1 fall 3 {{backup}}/& on-marked-down shutdown-sessions/' feeds/kenzo/luci-app-passwall/root/usr/share/passwall/haproxy.lua
 #sed -i 's/server \$remark:\$bport \$bip:\$bport weight \$lbweight check inter 1000 rise 30 fall 3 \$bbackup/& on-marked-down shutdown-sessions/' feeds/kenzo/luci-app-passwall/root/usr/share/passwall/haproxy.lua
 
 #socks健康检测
