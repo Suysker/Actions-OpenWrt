02fd033ec54c8c3969bd118ef1019fa8f1f6ef09
Suysker <61983976+Suysker@users.noreply.github.com>
2023-05-10 01:18:35 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,10 +16,12 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+sed -i '1i src-git small https://github.com/kenzok8/small' feeds.conf.default
 sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '1i src-git sbwml https://github.com/sbwml/luci-app-mosdns' feeds.conf.default
 sed -i '1i src-git xiaorouji https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
 sed -i '1i src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;luci' feeds.conf.default
 
-sed -i '1a src-git small https://github.com/kenzok8/small' feeds.conf.default
-sed -i '1a src-git opluci https://git.openwrt.org/project/luci.git' feeds.conf.default
+
+echo 'src-git opluci https://git.openwrt.org/project/luci.git' >>feeds.conf.default
+#sed -i '1a src-git opluci https://git.openwrt.org/project/luci.git' feeds.conf.default
