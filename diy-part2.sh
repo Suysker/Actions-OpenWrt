4319d9a086a7eb3aa42f445d15ec1f8cb9788bd7
Suysker <61983976+Suysker@users.noreply.github.com>
2023-11-06 22:47:34 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -85,6 +85,8 @@ sed -i '/--no-check-update/d' feeds/kenzo/adguardhome/files/adguardhome.init
 #sed -i 's/PROG=.*/PROG=\/etc\/AdGuardHome\/AdGuardHome/' feeds/kenzo/adguardhome/files/adguardhome.init
 
 sed -i 's/PKG_HASH:=.*/PKG_HASH:=skip/' feeds/kenzo/adguardhome/Makefile
+sed -i '/^\t\$(call Build\/Prepare\/Default)/a \\tif [ -d "$(BUILD_DIR)\/AdGuardHome-$(PKG_VERSION)" ]; then \\\n\t\tmv "$(BUILD_DIR)\/AdGuardHome-$(PKG_VERSION)\/"* "$(BUILD_DIR)\/adguardhome-$(PKG_VERSION)\/"; \\\n\tfi' feeds/kenzo/adguardhome/Makefile
+
 
 #mosdns默认配置
 #取消默认IPV4
